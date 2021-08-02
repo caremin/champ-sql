fe0dff09b09cd16d1cf3b5988a7d5ef5acfeedaa
icm-marcelo <79075994+icm-marcelo@users.noreply.github.com>
2021-08-02 08:02:27 +0800

    Delete contactview_clinic.sql

--- contactview_clinic.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE VIEW public.contactview_clinic
-AS SELECT cmd.uuid,
-    cmd.name,
-    chw.uuid AS chw_uuid,
-    cmd.reported AS created
-   FROM contactview_metadata cmd
-     JOIN contactview_chw chw ON cmd.parent_uuid = chw.area_uuid
-  WHERE cmd.type = 'clinic'::text;
